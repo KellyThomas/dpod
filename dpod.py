cccc1d3b9d954ad623bbce58467455d9b145ae44
Kelly Thomas <kelly.thomas@hotmail.com.au>
2024-02-11 19:59:51 +0800

    init

---
+++ dpod.py
@@ -0,0 +1,105 @@
+"""
+    (c) 2024 Kelly Thomas  
+    This Source Code Form is subject to the terms of the Mozilla
+    Public License, v. 2.0. If a copy of the MPL was not distributed
+    with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+import feedparser
+from slugify import slugify
+import os
+from datetime import datetime as dt
+import shutil
+import urllib.request
+import yaml
+import argparse
+
+
+mime_extentions = {"audio/mpeg": "mp3"}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        epilog="Either the --url or --urls must be provided"
+    )
+    parser.add_argument(
+        "--home_dir",
+        help="Path to download home directory, defaults to current directory",
+    )
+    parser.add_argument("--url", help="URL for a single RSS feed")
+    parser.add_argument(
+        "--urls", help="Path for text file listing URLs for a multiple RSS feeds"
+    )
+    args = parser.parse_args()
+
+    home_dir = args.home_dir if args.home_dir else "."
+    home_dir = os.path.abspath(home_dir)
+
+    urls = set()
+    if args.url:
+        urls.add(args.url)
+    if args.urls:
+        with open(args.urls, "r") as f:
+            for url in f:
+                urls.add(url)
+
+    if len(urls) == 0:
+        print("No URL(s) provided")
+        parser.print_help()
+        exit(1)
+
+    download_podcasts(list(urls), home_dir)
+
+
+def download_podcasts(urls: list, home_dir: str) -> None:
+    for url in urls:
+        fp = feedparser.parse(url)
+        podslug = slugify(fp.feed.title)
+        podcast_dir = os.path.join(home_dir, podslug)
+        if not os.path.isdir(podcast_dir):
+            os.makedirs(podcast_dir)
+
+        for e in fp.entries:
+            download_podcast_episode(e, podcast_dir)
+
+
+def download_podcast_episode(episode, podcast_dir) -> None:
+    episode_date = parse_rss_date(episode.published).strftime("%Y%m%d")
+    episode_base_name = f"{episode_date}-{slugify(episode.title)}"
+    episode_link = [l for l in episode.links if l["rel"] == "enclosure"][0]
+    episode_ext = mime_extentions[episode_link["type"]]
+    episode_audio_file = os.path.join(podcast_dir, f"{episode_base_name}.{episode_ext}")
+    episode_txt_file = os.path.join(podcast_dir, f"{episode_base_name}.txt")
+
+    if os.path.isfile(episode_audio_file):
+        print(f"found: {episode_audio_file}")
+    else:
+        with urllib.request.urlopen(episode_link["href"]) as response:
+            with open(episode_audio_file, "wb") as af:
+                shutil.copyfileobj(response, af)
+                print(f"downloaded: {episode_audio_file}")
+
+    with open(episode_txt_file, "w") as tf:
+        metadata = {
+            "title": episode.title,
+            "duration": episode.itunes_duration,
+            "sumary": episode.summary,
+        }
+        tf.write(yaml.dump(metadata))
+
+
+def parse_rss_date(datestring: str) -> dt:
+    # see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+    try:
+        return dt.strptime(datestring, "%a, %d %b %Y %H:%M:%S %z")
+    except:
+        pass
+    try:
+        return dt.strptime(datestring, "%a, %d %b %Y %H:%M:%S %Z")
+    except:
+        pass
+    raise ValueError(f"ERROR WITH DATESTRING: {datestring}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,66 @@
+# dpod
+
+Podcast downloader
+
+## Setup
+
+1. Requirements:
+   * recent Python 3.x, tested with 3.10
+
+2. Clone repo
+
+3. Create venv and install required packages:
+```
+    python -m venv venv
+    ./venv/Scripts/activate
+    pip install -r requirements.txt
+```
+
+## Usage
+
+```
+> python .\dpod.py --help
+usage: dpod.py [-h] [--home_dir HOME_DIR] [--url URL] [--urls URLS]
+
+options:
+  -h, --help           show this help message and exit
+  --home_dir HOME_DIR  Path to download home directory, defaults to current directory
+  --url URL            URL for a single RSS feed
+  --urls URLS          Path for text file listing URLs for a multiple RSS feeds
+
+Either the --url or --urls must be provided
+```
+
+### Single URL
+
+```
+>python .\dpod.py --url https://example.com/path/to/podcast.rss
+```
+
+### Multiple URLS
+
+If `podcasts.txt` contains a list of podcast RSS URLS:
+```
+https://example.com/path/to/podcast.rss
+https://example.com/path/to/another/podcast.rss
+```
+Then they can be downloaded as a batch.
+```
+>python .\dpod.py --url /local/path/to/podcasts.txt
+```
+### Download output
+
+1. HOME_DIR will default to current working directory but can be set with a parameter.
+2. In the home directory a podcast directory will be created with the slugified title of the podcast.
+3. In the podcast directory files with will be created with a leading `YYYYMMDD` timestamp and a slugified title of the episode.
+
+For example:
+
+````
+HOME_DIR/podcast-title/20231231-episode-title.mp3
+HOME_DIR/podcast-title/20240101-another-episode-title.mp3
+HOME_DIR/another-podcast-title/20231231-episode-title.mp3
+HOME_DIR/another-podcast-title/20240101-another-episode-title.mp3
+````
+
+Each episode also has a matching *.txt with brief episode metadata.
